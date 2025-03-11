a7e8a63c5bd0785766ac7a21f8169a2247db41ce
unknown <Paulo-05-mc>
2025-03-10 21:06:31 -0400

    Initial commit

---
+++ ProductsAPI/Data/AppDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductsAPI.Models;
+namespace ProductsAPI.Data;
+
+public class AppDDbContext(DbContextOptions<AppDDbContext> options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; }
+}
---
+++ ProductsAPI/Models/Product.cs
@@ -0,0 +1,6 @@
+﻿namespace ProductsAPI.Models;
+
+public class Product
+{
+    
+}
